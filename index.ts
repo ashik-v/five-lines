3cb54aed7233468eed05008f1fdfe62aa607cd26
Ashik Varghese <ashikv@nulogy.com>
2022-11-05 15:53:56 -0400

    Inline method

--- index.ts
+++ index.ts
@@ -449,14 +449,10 @@ function update() {
   updateMap();
 }
 
-function colorOfTile(y: number, x: number, g: CanvasRenderingContext2D) {
-  map[y][x].color(g);
-}
-
 function drawMap(g: CanvasRenderingContext2D) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      colorOfTile(y, x, g);
+      map[y][x].color(g);
 
       if (!map[y][x].isAir() && !map[y][x].isPlayer())
         g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
