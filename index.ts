1f8d9315cbccb17bfc9193666d15ae318a4904c7
Ashik Varghese <ashikv@nulogy.com>
2022-11-07 09:52:27 -0500

    Extract methods from update()

--- index.ts
+++ index.ts
@@ -80,7 +80,7 @@ function moveVertical(dy: number) {
   }
 }
 
-function update() {
+function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
     if (current === Input.LEFT)
@@ -92,15 +92,17 @@ function update() {
     else if (current === Input.DOWN)
       moveVertical(1);
   }
+}
 
+function updateMap() {
   for (let y = map.length - 1; y >= 0; y--) {
     for (let x = 0; x < map[y].length; x++) {
       if ((map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_STONE;
         map[y][x] = Tile.AIR;
       } else if ((map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-        && map[y + 1][x] === Tile.AIR) {
+          && map[y + 1][x] === Tile.AIR) {
         map[y + 1][x] = Tile.FALLING_BOX;
         map[y][x] = Tile.AIR;
       } else if (map[y][x] === Tile.FALLING_STONE) {
@@ -110,6 +112,11 @@ function update() {
       }
     }
   }
+}
+
+function update() {
+  handleInputs();
+  updateMap();
 }
 
 function drawMap(g: CanvasRenderingContext2D) {
