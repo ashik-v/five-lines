a74496592e6857d3582a4f8e70e2cd61f01b6ee1
Ashik Varghese <ashikv@nulogy.com>
2022-11-16 22:04:40 -0500

    Refactor: Simplify conditional by factoring out common code that assigns to this.falling

--- index.ts
+++ index.ts
@@ -23,12 +23,11 @@ class FallStrategy {
   }
 
   update(tile: Tile, x: number, y: number) {
-    if (map[y + 1][x].isAir()) {
-      this.falling = new Falling();
+    this.falling = map[y + 1][x].isAir() ? new Falling() : new Resting()
+
+    if (this.falling.isFalling()) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
-    } else {
-      this.falling = new Resting();
     }
   }
 }
