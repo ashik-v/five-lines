009e08127d0bcc7283ad3886126eed3cd960db0d
Ashik Varghese <ashikv@nulogy.com>
2022-11-16 22:06:02 -0500

    Refactor: Extract method

--- index.ts
+++ index.ts
@@ -25,6 +25,10 @@ class FallStrategy {
   update(tile: Tile, x: number, y: number) {
     this.falling = map[y + 1][x].isAir() ? new Falling() : new Resting()
 
+    this.drop(tile, x, y);
+  }
+
+  private drop(tile: Tile, x: number, y: number) {
     if (this.falling.isFalling()) {
       map[y + 1][x] = tile;
       map[y][x] = new Air();
