93787191554361f91406cdda8caa8fce24e7af6d
Ashik Varghese <ashikv@nulogy.com>
2022-11-17 21:58:35 -0500

    Introduce Strategy Pattern: Unify methods

--- index.ts
+++ index.ts
@@ -217,11 +217,11 @@ class Key1 implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    removeLock1();
+    remove(new RemoveLock1());
     moveToTile(playerx + dx, playery);
   }
   moveVertical(dy: number) {
-    removeLock1();
+    remove(new RemoveLock1());
     moveToTile(playerx, playery + dy);
   }
   update(x: number, y: number) { }
@@ -236,11 +236,11 @@ class Key2 implements Tile {
     g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
   }
   moveHorizontal(dx: number) {
-    removeLock2();
+    remove(new RemoveLock2());
     moveToTile(playerx + dx, playery);
   }
   moveVertical(dy: number) {
-    removeLock2();
+    remove(new RemoveLock2());
     moveToTile(playerx, playery + dy);
   }
   update(x: number, y: number) { }
@@ -345,28 +345,26 @@ function transformMap() {
 
 let inputs: Input[] = [];
 
-class RemoveStrategy {
+interface RemoveStrategy {
+  check(tile: Tile): boolean
+}
+
+class RemoveLock1 implements RemoveStrategy {
   check(tile: Tile) {
     return tile.isLock1();
   }
 }
 
-
-function removeLock1() {
-  let shouldRemove = new RemoveStrategy()
-  for (let y = 0; y < map.length; y++) {
-    for (let x = 0; x < map[y].length; x++) {
-      if (shouldRemove.check(map[y][x])) {
-        map[y][x] = new Air();
-      }
-    }
+class RemoveLock2 implements RemoveStrategy {
+  check(tile: Tile) {
+    return tile.isLock2();
   }
 }
 
-function removeLock2() {
+function remove(shouldRemove: RemoveStrategy) {
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x].isLock2()) {
+      if (shouldRemove.check(map[y][x])) {
         map[y][x] = new Air();
       }
     }
